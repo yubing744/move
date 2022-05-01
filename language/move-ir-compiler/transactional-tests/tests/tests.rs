cf4aeb3c0a9d4b5b4e1baf171ddb5c3e24ca6442
Owen Wu <yubing744@163.com>
2022-05-01 14:13:34 +0000

    feat: update move-ir-compiler tests

--- language/move-ir-compiler/transactional-tests/tests/tests.rs
+++ language/move-ir-compiler/transactional-tests/tests/tests.rs
@@ -4,4 +4,4 @@
 pub const TEST_DIR: &str = "tests";
 use move_transactional_test_runner::vm_test_harness::run_test;
 
-datatest_stable::harness!(run_test, TEST_DIR, r".*crlf\.mvir$");
+datatest_stable::harness!(run_test, TEST_DIR, r".*\.mvir$");
